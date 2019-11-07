8f141d104a8681e7726bb124bdb79567973a7cb8
Josh Walters <39789483+joshnwalters@users.noreply.github.com>
2019-11-07 12:45:40 -1000

    Skeleton for charts created
    Changed first card to semantic statisc
    Added chart skeleton to remaining cards(with errors)

--- src/Components/Dashboard/DashView.js
+++ src/Components/Dashboard/DashView.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Statistic } from 'semantic-ui-react';
+import { Pie, Bar } from 'react-chartjs-2'
 import style from './dashboard.scss';
 
 export default function DashView() {
+
+
   return (
     <div className={style.container}>
       <div className={style.dashView}>
+        <Statistic size='small'>
+          <Statistic.Value>
+            Turnaround Time
+          </Statistic.Value>
+          <Statistic.Label>Average Time Between Uses</Statistic.Label>
+
+        </Statistic>
         <Card.Group>
           <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
-            <Card.Content>  
+            <Card.Content>
               <Card.Header>
-                Hello There
+                Some Data
               </Card.Header>
+              let BarChart = require("react-chartjs").Bar;
+
+              let MyComponent = React.createClass({
+              render: function() {
+              return <BarChart data={chartData} options={chartOptions}/>
+            }
+            });
+            </Card.Content>
+          </Card>
+          <Card color='red'>
+            <Card.Content>
+              <Card.Header>
+                Some more Data
+              </Card.Header>
+              let PieChart = require("react-chartjs").Pie;
+
+              let MyComponent = React.createClass({
+              render: function() {
+              return <PieChart data={chartData} options={chartOptions}/>
+            }
+            });
             </Card.Content>
           </Card>
         </Card.Group>
