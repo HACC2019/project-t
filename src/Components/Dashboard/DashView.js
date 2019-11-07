8000368417e6e366618bee2955e591bf63f6fecc
Matthew Kim <45888473+mattkim009@users.noreply.github.com>
2019-11-07 01:53:42 -1000

    testing cards

--- src/Components/Dashboard/DashView.js
+++ src/Components/Dashboard/DashView.js
@@ -15,6 +15,70 @@ export default function DashView() {
               </Card.Header>
             </Card.Content>
           </Card>
+          <Card color='red'>
+            <Image src="https://via.placeholder.com/150" />
+            <Card.Content>  
+              <Card.Header>
+                Hello There 1
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          <Card color='red'>
+            <Image src="https://via.placeholder.com/150" />
+            <Card.Content>  
+              <Card.Header>
+                Hello There 2
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          <Card color='red'>
+            <Image src="https://via.placeholder.com/150" />
+            <Card.Content>  
+              <Card.Header>
+                Hello There 3
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          <Card color='red'>
+            <Image src="https://via.placeholder.com/150" />
+            <Card.Content>  
+              <Card.Header>
+                Hello There 4
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          <Card color='red'>
+            <Image src="https://via.placeholder.com/150" />
+            <Card.Content>  
+              <Card.Header>
+                Hello There 5
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          <Card color='red'>
+            <Image src="https://via.placeholder.com/150" />
+            <Card.Content>  
+              <Card.Header>
+                Hello There 6
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          <Card color='red'>
+            <Image src="https://via.placeholder.com/150" />
+            <Card.Content>  
+              <Card.Header>
+                Hello There 7
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          <Card color='red'>
+            <Image src="https://via.placeholder.com/150" />
+            <Card.Content>  
+              <Card.Header>
+                Hello There 8
+              </Card.Header>
+            </Card.Content>
+          </Card>
         </Card.Group>
       </div>
     </div>
