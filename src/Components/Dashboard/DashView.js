dcc5f6e21a51f2891a18deb95df9b6eca31ed23b
Matthew Kim <45888473+mattkim009@users.noreply.github.com>
2019-11-09 00:11:36 -1000

    removed some cards from the dashboard

--- src/Components/Dashboard/DashView.js
+++ src/Components/Dashboard/DashView.js
@@ -8,19 +8,7 @@ export default function DashView() {
       <div className={style.dashView}>
         <Card.Group>
           <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
             <Card.Content>  
-              <Card.Header>
-                Hello There
-              </Card.Header>
-            </Card.Content>
-          </Card>
-          <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
-            <Card.Content>  
-              <Card.Header>
-                Hello There 1
-              </Card.Header>
             </Card.Content>
           </Card>
           <Card color='red'>
@@ -44,38 +32,6 @@ export default function DashView() {
             <Card.Content>  
               <Card.Header>
                 Hello There 4
-              </Card.Header>
-            </Card.Content>
-          </Card>
-          <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
-            <Card.Content>  
-              <Card.Header>
-                Hello There 5
-              </Card.Header>
-            </Card.Content>
-          </Card>
-          <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
-            <Card.Content>  
-              <Card.Header>
-                Hello There 6
-              </Card.Header>
-            </Card.Content>
-          </Card>
-          <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
-            <Card.Content>  
-              <Card.Header>
-                Hello There 7
-              </Card.Header>
-            </Card.Content>
-          </Card>
-          <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
-            <Card.Content>  
-              <Card.Header>
-                Hello There 8
               </Card.Header>
             </Card.Content>
           </Card>
