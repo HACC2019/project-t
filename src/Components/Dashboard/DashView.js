017408eb9dfe8c813264042e9cd7792824198bf7
Matthew Kim <45888473+mattkim009@users.noreply.github.com>
2019-11-09 14:35:58 -1000

    applied some of jeff's changes

--- src/Components/Dashboard/DashView.js
+++ src/Components/Dashboard/DashView.js
@@ -1,30 +1,35 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Statistic } from 'semantic-ui-react';
 import style from './dashboard.scss';
+import SomeBarChart from './Charts/SomeBarChart';
+import SomePieChart from './Charts/SomePieChart';
 
 export default function DashView() {
   return (
     <div className={style.container}>
       <div className={style.dashView}>
+        <Statistic size='small'>
+            <Statistic.Value>
+              Turnaround Time
+            </Statistic.Value>
+            <Statistic.Label>Average Time Between Uses</Statistic.Label>
+
+        </Statistic>
         <Card.Group>
-          <Card color='red'>
-            <Card.Content>  
+        <Card color='red'> 
+            <Card.Content>
+              <Card.Header>
+                Some Data
+              </Card.Header>
+              <SomeBarChart />
             </Card.Content>
           </Card>
           <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
-            <Card.Content>  
+            <Card.Content>
               <Card.Header>
-                Hello There 2
+                Some more Data
               </Card.Header>
-            </Card.Content>
-          </Card>
-          <Card color='red'>
-            <Image src="https://via.placeholder.com/150" />
-            <Card.Content>  
-              <Card.Header>
-                Hello There 3
-              </Card.Header>
+              <SomePieChart />
             </Card.Content>
           </Card>
           <Card color='red'>
