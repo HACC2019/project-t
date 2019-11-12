0e903ced166ca0aeb5ef19a2cf056512fa70caf0
Tyler Chong <tcchong@hawaii.edu>
2019-11-11 17:07:37 -1000

    Add 10 mile radius to newTrips

--- src/Components/Map/MapComponent.jsx
+++ src/Components/Map/MapComponent.jsx
@@ -153,37 +153,56 @@ class MapComponent extends Component {
         ]
       });
 
-      let smallestDistance = 25;
-      let closestStation = null;
+      function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1); 
+        var a = 
+          Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+          Math.sin(dLon/2) * Math.sin(dLon/2)
+          ; 
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c; // Distance in km
+        return d;
+      }
+      
+      function deg2rad(deg) {
+        return deg * (Math.PI/180)
+      }
+
+      let validRadius = 16.0934; // 10 Miles in Kilometers
+      let validStations = [];
+
       // Find the closest charging station to generate cars (trips) from
       for (let station of this.state.data.chargeStations) {
-        let a = info.coordinate[0] - station.Longitude;
-        let b = info.coordinate[1] - station.Latitude;
+        let distance = getDistanceFromLatLonInKm(info.coordinate[1], info.coordinate[0], station.Latitude, station.Longitude);
 
-        let distance = Math.sqrt( a*a + b*b );
-        if (distance < smallestDistance) {
-          smallestDistance = distance;
-          closestStation = station;
+        if (distance <= validRadius) {
+          validStations.push(station);
         }
       }
 
+      const newTrips = this.state.newTrips;
+      for (let station of validStations) {
+        newTrips.push({
+          Latitude: info.coordinate[1],
+          Longitude: info.coordinate[0],
+          stationID: newStationID,
+          path: [
+            [station.Longitude, station.Latitude],
+            [info.coordinate[0], info.coordinate[1]] 
+          ],
+          timestamps: [
+            0,
+            1000
+          ]
+        });
+      }
+
+
       this.setState({
-        newTrips: [
-          ...this.state.newTrips,
-          {
-            Latitude: info.coordinate[1],
-            Longitude: info.coordinate[0],
-            stationID: newStationID,
-            path: [
-              [closestStation.Longitude, closestStation.Latitude],
-              [info.coordinate[0], info.coordinate[1]] 
-            ],
-            timestamps: [
-              0,
-              1000
-            ]
-          }
-        ]
+        newTrips: newTrips
       });
     }
   }
@@ -314,11 +333,11 @@ class MapComponent extends Component {
         data: [trips],
         getPath: d => d.path,
         getTimestamps: d => d.timestamps,
-        getColor: d => this.choose([[253, 128, 93], [75, 218, 250]]),
+        getColor: d => [253, 128, 93],
         opacity: 0.5,                                                                                                        
         widthMinPixels: 2,
         rounded: true,
-        trailLength: 10,
+        trailLength: 50000,
         currentTime: this.state.time,
         shadowEnabled: false
       }));
