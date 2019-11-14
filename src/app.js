6fac983d6d2cd0e5001a45182ce13bcdb22f15b5
Ethan <epchow@hawaii.edu>
2019-11-13 20:12:07 -1000

    Changed initial dashboard size and map view state

--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ class App extends Component {
             />
             <div style={{ display: 'inline-flex', flexDirection: 'column', width: '100%'}}>
               <Resizable className={style.box}
-                         defaultSize={{height: 300}}
+                         defaultSize={{height: 388}}
                          minHeight={'20%'}
                          enable={{bottom: true}}
               >
